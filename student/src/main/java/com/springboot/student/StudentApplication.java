b0be11ffa518982dea01edda699ef678d9a2a9ce
whibatsu <whibatsu.helvantriyudo@ai.astra.co.id>
2024-07-16 08:49:13 +0700

    CommandLineRunner, Initializer for JPA & MySQL

---
+++ student/src/main/java/com/springboot/student/StudentApplication.java
@@ -0,0 +1,22 @@
+package com.springboot.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class StudentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args){
+
+		return runner -> {
+			System.out.println("cmd runner triggered");
+		};
+	}
+}
